0dfa2cfcfc67e8cc524ffcc10a163b015eec8cbf
Kanstantsin Valeitsenak <kanstantsin.valeitsenak@sap.com>
2026-01-20 02:29:25 +0300

    ops

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,15 +12,7 @@ buildscript {
 	dependencies {
 		classpath("com.android.tools.build:gradle:8.6.0")
 		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Metadata.Versions.kotlinVersion}")
-
 		classpath("org.jetbrains.kotlin.plugin.compose:org.jetbrains.kotlin.plugin.compose.gradle.plugin:${Metadata.Versions.kotlinVersion}")
-		if (project.hasProperty("vulas")) {
-			classpath("com.sap.research.security.vulas:plugin-gradle:3.0.12") {
-				isChanging = true
-			}
-		}
-
-		classpath("nl.neotech.plugin:android-root-coverage-plugin:1.8.0")
 	}
 }
 
